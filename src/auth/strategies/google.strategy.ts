59c6b419af87f4820af1f86cf7b9a44c32dccc2a
mouloud240 <m.hasrane@esi-sba.dz>
2025-02-24 00:12:26 +0100

    Fixed google redirect

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -3,14 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Profile, Strategy } from 'passport-google-oauth20';
 import { VerifiedCallback } from 'passport-jwt';
-import { async } from 'rxjs';
 @Injectable()
 export class googleStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
       clientID: configService.getOrThrow('GOOGLE_CLIENT_ID'),
       clientSecret: configService.getOrThrow('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      callbackURL: configService.getOrThrow('GOOGLE_CALLBACK_URL'),
       passReqToCallback: false,
       scope: ['email', 'profile'],
     });
--- src/auth/test.guard.ts
+++ src/auth/test.guard.ts
@@ -1,24 +1,29 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { JwtService } from "@nestjs/jwt";
-import { Observable } from "rxjs";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class testGuard implements CanActivate{
-  constructor(private readonly jwtService:JwtService,private readonly config:ConfigService){}
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const jwt=context.switchToHttp().getRequest().headers['authorization']
-    if (!jwt) return false
-    const token=jwt.split(' ')[1]
+export class testGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const jwt = context.switchToHttp().getRequest().headers['authorization'];
+    if (!jwt) return false;
+    const token = jwt.split(' ')[1];
     try {
-      console.log(token)
-      const tt= this.jwtService.verify(token,{
-        secret:this.config.get('ACCESS_TOKEN_SECRET')
-      })
-      return true
+      console.log(token);
+      const tt = this.jwtService.verify(token, {
+        secret: this.config.get('ACCESS_TOKEN_SECRET'),
+      });
+      return true;
     } catch (e) {
-      console.log(e)
-      return false
-    }}
-
+      console.log(e);
+      return false;
+    }
+  }
 }
